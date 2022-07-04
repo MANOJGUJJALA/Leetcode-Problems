541e635e3cc43303853f87f5f44927cd9cd27747
MANOJGUJJALA <34826798+MANOJGUJJALA@users.noreply.github.com>
2022-07-04 11:36:34 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        a=[]
-        f=0
+        # nums.sort()
+        l={}
+        h=len(nums)-1
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    f=1
-                    a.append(i)
-                    a.append(j)
-                    break
-            if f==1:
-                break
-                
-        return(a)
-         
+            if target-nums[i] in l:
+                return [i,l[target-nums[i] ]]
+            else:
+                l[nums[i]]=i
+        return [-1,-1]
+        
+        
+        
