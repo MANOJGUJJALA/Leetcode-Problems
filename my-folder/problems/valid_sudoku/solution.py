8fcad24e5b9199ed8b07697efb3f6cd9d4afbb26
MANOJGUJJALA <34826798+MANOJGUJJALA@users.noreply.github.com>
2022-12-20 07:48:08 +0000

    Sync LeetCode submission - Valid Sudoku (python3)

--- my-folder/problems/valid_sudoku/solution.py
+++ my-folder/problems/valid_sudoku/solution.py
@@ -1,19 +1,21 @@
+from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        r=collections.defaultdict(set)
-        c=collections.defaultdict(set)
-        s=collections.defaultdict(set)
-        for i in range(9):
-            for j in range(9):
-                if board[i][j]==".":
+
+        r,c=defaultdict(set),defaultdict(set)
+        sub=defaultdict(set)
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+
+                if board[i][j]=='.':
                     continue
                 else:
-                    if board[i][j] in r[i] or board[i][j] in c[j] or board[i][j] in s[(i//3,j//3)]:
+                    if board[i][j] in r[i] or board[i][j] in c[j] or board[i][j] in sub[(i//3,j//3)] :
                         return False
-                    
                     r[i].add(board[i][j])
                     c[j].add(board[i][j])
-                    s[(i//3,j//3)].add(board[i][j])
+
+                    sub[(i//3,j//3)].add(board[i][j])
         return True
-        
+
