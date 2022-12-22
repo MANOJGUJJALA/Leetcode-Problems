d99e2758d72fcf4ea0917fc95f7fd1144ad76ddf
MANOJGUJJALA <34826798+MANOJGUJJALA@users.noreply.github.com>
2022-12-22 06:09:38 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- my-folder/problems/permutation_in_string/solution.py
+++ my-folder/problems/permutation_in_string/solution.py
@@ -1,43 +1,38 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # s1=list(set(list(s1)))
-        # s2=list(set(list(s2)))
-        # print("--",s1,s2)
-        c=0
-        a=[0]*26
-        a=[0]*26
-        p={}
-        q={}
+
         if len(s2)<len(s1):
             return False
+
+
+        p={}
+        q={}
+
         for i in s1:
             if i in p:
                 p[i]+=1
             else:
                 p[i]=1
+        
         for i in range(len(s1)):
             if s2[i] in q:
                 q[s2[i]]+=1
             else:
                 q[s2[i]]=1
+        
         if p==q:
             return True
-        
-        h=0
-        # print("--",p,q)
-        
+        z=0
         for i in range(len(s1),len(s2)):
-            
-            q[s2[h]]-=1
-            if q[s2[h]]==0:
-                q.pop(s2[h])
+            q[s2[z]]-=1
+            if q[s2[z]]<=0:
+                q.pop(s2[z])
+            z+=1
             if s2[i] in q:
                 q[s2[i]]+=1
             else:
                 q[s2[i]]=1
             if p==q:
                 return True
-            h+=1
-   
-        # print("--",q)
         return False
+
