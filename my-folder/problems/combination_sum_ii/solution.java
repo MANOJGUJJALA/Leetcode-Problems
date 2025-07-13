b135580182d6802dc333c3661e357538af2bfed0
MANOJGUJJALA <34826798+MANOJGUJJALA@users.noreply.github.com>
2025-07-13 06:44:19 +0000

    Sync LeetCode submission - Combination Sum II (java)

--- my-folder/problems/combination_sum_ii/solution.java
+++ my-folder/problems/combination_sum_ii/solution.java
@@ -1,24 +1,29 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-        Arrays.sort(candidates); // Sort to handle duplicates easily
-        backtrack(candidates, target, 0, new ArrayList<>(), results);
-        return results;
-    }
 
-    private void backtrack(int[] candidates, int target, int start, List<Integer> currentCombination, List<List<Integer>> results) {
-        if (target == 0) {
-            results.add(new ArrayList<>(currentCombination));
+    public void dfs(List<List<Integer>>r,List<Integer>path,int s,int st,int t,int[]nums){
+        if(s==t){
+            r.add(new ArrayList<>(path));
             return;
         }
-
-        for (int i = start; i < candidates.length && candidates[i] <= target; i++) {
-            if (i > start && candidates[i] == candidates[i - 1]) {
-                continue; // Skip duplicates
-            }
-            currentCombination.add(candidates[i]);
-            backtrack(candidates, target - candidates[i], i + 1, currentCombination, results);
-            currentCombination.remove(currentCombination.size() - 1); // Backtrack
+        for(int i=st;i<nums.length;i++){
+            if (i > st &&  nums[i] == nums[i - 1]) continue;
+            if (s + nums[i] > t) break;
+            path.add(nums[i]);
+            s+=nums[i];
+            dfs(r,path,s,i+1,t,nums);
+            s-=path.get(path.size()-1);
+            path.remove(path.size()-1);
         }
+        
+
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>>r=new ArrayList<>();
+        List<Integer>path=new ArrayList<>();
+        int s=0,st=0;
+        Arrays.sort(candidates);
+        dfs(r,path,s,st,target,candidates);
+        return r;
+        
     }
 }
