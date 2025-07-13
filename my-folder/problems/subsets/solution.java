f9d006e7c017ba2e6b4532cef685108c8b7402e6
MANOJGUJJALA <34826798+MANOJGUJJALA@users.noreply.github.com>
2025-07-13 05:56:33 +0000

    Sync LeetCode submission - Subsets (java)

--- my-folder/problems/subsets/solution.java
+++ my-folder/problems/subsets/solution.java
@@ -1,15 +1,19 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<>()); // Add the empty subset
-        for (int num : nums) {
-            int size = result.size();
-            for (int i = 0; i < size; i++) {
-                List<Integer> subset = new ArrayList<>(result.get(i));
-                subset.add(num);
-                result.add(subset);
-            }
+
+    public void dfs(List<Integer>path,int st,List<List<Integer>>r,int[] nums){
+        r.add(new ArrayList<>(path));
+        for(int i=st;i<nums.length;i++){
+            path.add(nums[i]);
+            dfs(path,i+1,r,nums);
+            path.remove(path.size()-1);
         }
-        return result;
+
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>>r=new ArrayList<>();
+        List<Integer>path=new ArrayList<>();
+        int st=0;
+        dfs(path,0,r,nums);
+        return r;
     }
 }
